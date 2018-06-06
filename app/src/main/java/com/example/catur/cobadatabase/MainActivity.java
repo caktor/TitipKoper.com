6d0ac4edda16bee78f944e3832efd39562cd3141
caktor <caturputra.ep@gmail.com>
2018-06-07 02:55:24 +0700

    Projek Database

---
+++ app/src/main/java/com/example/catur/cobadatabase/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.catur.cobadatabase;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvResultNama;
+    String resultNama;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        // untuk mendapatkan data dari activity sebelumnya, yaitu activity login.
+        Bundle extras = getIntent().getExtras();
+        if (extras != null)
+            resultNama = extras.getString("result_nama");
+        tvResultNama.setText(resultNama);
+
+    }
+
+    private void initComponents() {
+
+        tvResultNama = (TextView) findViewById(R.id.tvResultNama);
+
+    }
+}
---
+++ app/src/main/java/com/example/catur/cobadatabase/RegisterActivity.java
@@ -0,0 +1,107 @@
+package com.example.catur.cobadatabase;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.catur.cobadatabase.apihelper.BaseApiService;
+import com.example.catur.cobadatabase.apihelper.UtilsApi;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText etNama;
+    EditText etEmail;
+    EditText etPassword;
+    Button btnRegister;
+    ProgressDialog loading;
+
+    Context mContext;
+    BaseApiService mApiService;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        mContext = this;
+        mApiService = UtilsApi.getAPIService();
+
+        initComponents();
+    }
+
+    private void initComponents() {
+
+        etNama = (EditText) findViewById(R.id.etNama);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loading = ProgressDialog.show(mContext, null, "Harap Tunggu...", true, false);
+                requestRegister();
+            }
+
+            private void requestRegister() {
+
+                mApiService.loginRequest(etEmail.getText().toString(), etPassword.getText().toString())
+                .enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()){
+                            loading.dismiss();
+                            try {
+                                JSONObject jsonRESULTS = new JSONObject(response.body().string());
+                                if (jsonRESULTS.getString("error").equals("false")){
+                                    // Jika login berhasil maka data nama yang ada di response API
+                                    // akan diparsing ke activity selanjutnya.
+                                    Toast.makeText(mContext, "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
+                                    String nama = jsonRESULTS.getJSONObject("user").getString("nama");
+                                    Intent intent = new Intent(mContext, MainActivity.class);
+                                    intent.putExtra("result_nama", nama);
+                                    startActivity(intent);
+                                } else {
+                                    // Jika login gagal
+                                    String error_message = jsonRESULTS.getString("error_msg");
+                                    Toast.makeText(mContext, error_message, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            loading.dismiss();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("debug", "onFailure: ERROR > " + t.toString());
+                        loading.dismiss();
+                    }
+                });
+
+            }
+        });
+    }
+}
